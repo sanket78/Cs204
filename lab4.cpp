ec77f42b6f8d1293a7dba587e98e7e0a159b2f72
sanket78 <45785450+sanket78@users.noreply.github.com>
2019-08-31 20:52:29 +0530

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,223 +1,267 @@
-#include<bits/stdc++.h>
-#include<string>
+include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define REP(i,a,b) for(ll i=a;i<b;i++)
+#define REPI(i,a,b) for(ll i=b-1;i>=a;i--)
+#define PB push_back
 
-struct et 
-{ 
-    string value; 
-    et* left, *right; 
-}; 
-
-int toInt(string s)  
-{  
-    int num = 0;  
-     
-        for (int i=0; i<s.length(); i++)  
-            num = num*10 + (int(s[i])-48);  
-      
-    return num;  
-}  
-
-
-
-int eval(et* root)  
-{  
-    // empty tree  
-    if (!root)  
-        return 0;  
-  
-    // leaf node i.e, an integer  
-    if (!root->left && !root->right)  
-        return toInt(root->value);  
-  
-    // Evaluate left subtree  
-    int l_val = eval(root->left);  
-  
-    // Evaluate right subtree  
-    int r_val = eval(root->right);  
-   string w=root->value;
-    // Check which operator to apply  
-    if (w[0]=='+')  
-        return l_val+r_val;  
-  
-    if (w[0]=='-')  
-        return l_val-r_val;  
-  
-    if (w[0]=='/')  
-        return l_val*r_val; 
-
-    if (w[0]=='*')  
-        return l_val*r_val;   
-     if(w[0]=='^')
-         return pow(l_val,r_val);
-    return l_val/r_val;  
-}  
-
-  
-
-int isOperator(string s)
-{if(s[0]=='+'||s[0]=='*'||s[0]=='-'||s[0]=='/'||s[0]=='^')
-return(1);
-else
-return(0);}
-
-et* newNode(string v) 
-{ 
-    et *temp = new et; 
-    temp->left = temp->right = NULL; 
-    temp->value = v; 
-    return temp; 
-}; 
-
-et* constructTree(vector<string> j) 
-{ 
-    stack<et *> st; 
-    et *t, *t1, *t2; 
-  
-    // Traverse through every character of 
-    // input expression 
-    for (int i=0; i<j.size(); i++) 
-    { 
-        // If operand, simply push into stack 
-        if (!isOperator(j[i])) 
-        { 
-            t = newNode(j[i]); 
-            st.push(t); 
-        } 
-        else // operator 
-        { 
-            t = newNode(j[i]); 
-  
-            // Pop two top nodes 
-            t1 = st.top(); // Store top 
-            st.pop();      // Remove top 
-            t2 = st.top(); 
-            st.pop(); 
-  
-            //  make them children 
-            t->right = t1; 
-            t->left = t2; 
-  
-            // Add this subexpression to stack 
-            st.push(t); 
-        } 
-    } 
-  
-    //  only element will be root of expression 
-    // tree 
-    t = st.top(); 
-    st.pop(); 
-  
-    return t; 
-} 
-
-
-int prec(char c) 
-{ 
-    if(c == '^') 
-    return 3; 
-    else if(c == '*' || c == '/') 
-    return 2; 
-    else if(c == '+' || c == '-') 
-    return 1; 
-    else
-    return -1; 
-} 
-
-
-
-vector <string> intopo(string s)
-{ stack<string> st;
-string x;
-st.push(x);
-vector<string> ns;
-for(int i = 0; i < s.length(); i++) 
-    {  string q="";
-       string r;
-      while(s[i] >= '0' && s[i] <= '9')
-         {  
-            q.push_back(s[i]);
-            i++;  }
-          ns.push_back(q);
-         q.clear();
-
-
-        // If the scanned character is an operand, add it to output string. 
-        if((s[i] >= 'a' && s[i] <= 'z')||(s[i] >= 'A' && s[i] <= 'Z')) 
-        {string r="";
-         r.push_back(s[i]);
-         ns.push_back(r);}
-          r.clear();
-  
-        // If the scanned character is an ‘(‘, push it to the stack. 
-        if(s[i] == '(') 
-          {string r="";
-         r.push_back(s[i]);
-         st.push(r);}
-
-       r.clear();
-
-          string d="(";
-        // If the scanned character is an ‘)’, pop and to output string from the stack 
-        // until an ‘(‘ is encountered. 
-        if(s[i] == ')') 
-        { 
-            while(st.top() != x && st.top() !=d) 
-            { 
-                string c = st.top(); 
-                st.pop(); 
-               ns.push_back(c);
-            } 
-            if(st.top() == d) 
-            { 
-                string c = st.top(); 
-                st.pop(); 
-            } 
-        } 
-          
-        //If an operator is scanned 
-        if(s[i]=='+'||s[i]=='/'||s[i]=='*'||s[i]=='-'||s[i]=='^'){ string f=st.top();
-               char c=f[0];
-            while(st.top() != x && prec(s[i]) <= prec(c)) 
-            { 
-                string h = st.top(); 
-                st.pop(); 
-               char c=h[0];
-                ns.push_back(h);
-            } 
-            string r="";
-         r.push_back(s[i]);
-         st.push(r);
-        } }
-         while(st.top() != x) 
-           { 
-        string c = st.top(); 
-        st.pop(); 
-        ns.push_back(c);
-            } 
-               
-  return ns;
-    } 
-
-
-int main()
-{ int k,l;
-cin>>k;
-for(l=0;l<k;l++)
-{string o;
-cin>>o;
-vector<string> j = intopo(o);
-int i;
-et* a = constructTree(j) ;
-int num = eval(a);
-cout<<num<<endl;}
+ll prec(char c)
+{
+    switch(c)
+    {
+        case '+':
+            return 0;
+        case '-':
+            return 0;
+        case '*':
+            return 1;
+        case '/':
+            return 1;
+        case '^':
+            return 2;
+        default:
+            return -1;
+    }
+    return -1;
 }
 
+struct llchar
+{
+    bool f;
+    ll x;
+    char c;
+};
 
 
+struct node
+{
+    char c;
+    ll dat;
+    struct node *parent,*left,*right;
+};
 
+struct node *createNode()
+{
+    struct node *temp=(struct node *)malloc(sizeof(struct node));
+    return temp;
+}
 
+vector<llchar> itop(string s,bool &error)
+{
+    stack<char> st;
+    st.push('0');
+    ll n=s.size();
+    vector<llchar> tt;
+    bool flag=0;
+    REP(i,0,n)
+    {
+        if(s[i]<='9'&&s[i]>='0')
+        {
+            if(flag==1)
+            {
+                tt[tt.size()-1].x*=10;
+                tt[tt.size()-1].x+=(s[i]-'0');
+            }
+            else
+            {
+                llchar temp;
+                temp.x=(s[i]-'0');
+                temp.f=0;
+                tt.PB(temp);
+                flag=1;
+            }
+        }
+        else if(s[i] == '(')
+        {
+            st.push(s[i]);
+            flag=0;
+        }
+        else if(s[i] == ')')
+        {
+            while(st.top()!='0'&&st.top()!='(')
+            {
+                llchar temp;
+                temp.f=1;
+                temp.c=st.top();
+                st.pop();
+                tt.PB(temp);
+            }
+            if(st.top()=='(')
+            {
+                st.pop();
+            }
+            else
+            {
+                error=1;
+                return tt;
+            }
+            flag=0;
+        }
+        else if(prec(s[i])>=0)
+        {
+            while(st.top()!='0'&&prec(s[i])<=prec(st.top()))
+            {
+                if(s[i]=='^'&&st.top()=='^') break;
+                llchar temp;
+                temp.c=st.top();
+                temp.f=1;
+                st.pop();
+                tt.PB(temp);
+            }
+            st.push(s[i]);
+            flag=0;
+        }
+        else
+        {
+            error=1;
+            return tt;
+        }
 
+    }
+    while(st.top()!='0')
+    {
+        llchar temp;
+        temp.c=st.top();
+        temp.f=1;
+        st.pop();
+        tt.PB(temp);
+    }
+    return tt;
+}
+
+ll pow(ll x,ll y)
+{
+    ll t=1;
+    while(y--)
+    {
+        t*=x;
+    }
+    return t;
+}
+
+ll eval(struct node* root)
+{
+    if(root->left==NULL)
+    {
+        return root->dat;
+    }
+    else
+    {
+        switch (root->c)
+        {
+            case '+':
+                return eval(root->right)+eval(root->left);
+            case '-':
+                return eval(root->right)-eval(root->left);
+            case '*':
+                return eval(root->right)*eval(root->left);
+            case '/':
+                return eval(root->right)/eval(root->left);
+            case '^':
+                return pow(eval(root->right),eval(root->left));
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    ll q;
+    cin>>q;
+    while(q--)
+    {
+        ll n;
+        cin>>n;
+        while(n--)
+        {
+            string s0;
+            cin>>s0;
+            ll flag_unary=1;
+            string s;
+            REP(i,0,s0.size())
+            {
+                  if(flag_unary==1&&s0[i]=='-') s+="0";
+                  else if(s0[i]>='0'&&s0[i]<='9') flag_unary=0;
+                  else flag_unary=1;
+                  s+=s0[i];
+            }
+            struct node *root;
+            struct node *cur;
+            bool error=0;
+            vector<llchar> postfix=itop(s,error);
+            if(error==1)
+            {
+                cout<<"CANT BE EVALUATED\n";
+                continue;
+            }
+            REPI(i,0,postfix.size())
+            {
+                if(postfix[i].f==0&&i==postfix.size()-1)
+                {
+                    struct node *temp=createNode();
+                    temp->dat=postfix[i].x;
+                    temp->left=temp->right=temp->parent=NULL;
+                    cur=root=temp;
+                }
+                else if(postfix[i].f==1&&i==postfix.size()-1)
+                {
+                    struct node *temp=createNode();
+                    temp->c=postfix[i].c;
+                    temp->left=temp->right=temp->parent=NULL;
+                    cur=root=temp;
+                }
+                else if(postfix[i].f==0)
+                {
+                    if(cur->left==NULL)
+                    {
+                        struct node *temp=createNode();
+                        temp->dat=postfix[i].x;
+                        temp->left=temp->right=NULL;
+                        temp->parent=cur;
+                        cur->left=temp;
+                    }
+                    else
+                    {
+                        struct node *temp=createNode();
+                        temp->dat=postfix[i].x;
+                        temp->left=temp->right=NULL;
+                        temp->parent=cur;
+                        cur->right=temp;
+                        while(cur->right!=NULL)
+                        {
+                            if(cur->parent==NULL) break;
+                            cur=cur->parent;
+                        }
+                    }
+                }
+                else
+                {
+                    if(cur->left==NULL)
+                    {
+                        struct node *temp=createNode();
+                        temp->c=postfix[i].c;
+                        temp->left=temp->right=NULL;
+                        temp->parent=cur;
+                        cur->left=temp;
+                        cur=temp;
+                    }
+                    else
+                    {
+                        struct node *temp=createNode();
+                        temp->c=postfix[i].c;
+                        temp->left=temp->right=NULL;
+                        temp->parent=cur;
+                        cur->right=temp;
+                        cur=temp;
+                    }
+                }
+            }
+            cout<<eval(root)<<'\n';
+        }
+    }
+
+    return 0;
+}
 
 
 
